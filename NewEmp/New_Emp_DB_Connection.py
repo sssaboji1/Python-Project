b98f4dac9cf72b739d21fde253f3ae11f532e8fb
sssaboji1 <sssaboji1@gmail.com>
2025-12-11 19:12:36 +0530

    DB connecation All tables

--- NewEmp/New_Emp_DB_Connection.py
+++ NewEmp/New_Emp_DB_Connection.py
@@ -3,10 +3,31 @@ import mysql.connector
 mydb = mysql.connector.connect(host="localhost",user="root",password="sush123")
 
 
-mycursor = mydb.cursor()
+mycursor_employee = mydb.cursor()
+mycursor_Department = mydb.cursor()
+mycursor_Role = mydb.cursor()
+mycursor_Shift = mydb.cursor()
+mycursor_EmployeeShift = mydb.cursor()
 
 #mycursor.execute("info")
-mycursor.execute("Select * from EmployeeDB.employee")
+mycursor_employee .execute("Select * from EmployeeDB.Employee")
+for i in mycursor_employee:
+    print(i)        
 
-for i in mycursor:
-    print(i)
+mycursor_Department .execute("Select * from EmployeeDB.Department")
+for i in mycursor_Department:
+    print(i)      
+
+mycursor_Role .execute("Select * from EmployeeDB.Role")
+for i in mycursor_Role:     
+    print(i)    
+
+mycursor_Shift .execute("Select * from EmployeeDB.Shift")
+for i in mycursor_Shift:
+    print(i)    
+
+mycursor_EmployeeShift.execute("Select * from EmployeeDB.EmployeeShift")
+for i in mycursor_EmployeeShift:        
+    print(i)  
+
+    
